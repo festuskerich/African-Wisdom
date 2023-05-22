d65d76bdb9687c490c6286c52a1e8f79c7518066
festus.kerich <festus.kerich@cellulant.io>
2023-05-22 09:21:37 +0300

    a few changes

--- app/src/main/java/com/festuskerich/africanwisdom/auth/ui/login/LoginViewModel.java
+++ app/src/main/java/com/festuskerich/africanwisdom/auth/ui/login/LoginViewModel.java
@@ -6,10 +6,10 @@ import androidx.lifecycle.ViewModel;
 
 import android.util.Patterns;
 
+import com.festuskerich.africanwisdom.R;
 import com.festuskerich.africanwisdom.auth.data.LoginRepository;
 import com.festuskerich.africanwisdom.auth.data.Result;
 import com.festuskerich.africanwisdom.auth.data.model.LoggedInUser;
-import com.festuskerich.africanwisdom.auth.R;
 
 public class LoginViewModel extends ViewModel {
 
--- app/src/main/java/com/festuskerich/africanwisdom/ui/search/SearchFragment.java
+++ app/src/main/java/com/festuskerich/africanwisdom/ui/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.festuskerich.africanwisdom.ui.search;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.festuskerich.africanwisdom.auth.ui.login.LoginActivity;
 import com.festuskerich.africanwisdom.databinding.FragmentSearchBinding;
 
 public class SearchFragment extends Fragment {
@@ -27,6 +29,9 @@ public class SearchFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView.setOnClickListener(view ->{
+         startActivity(new Intent(getActivity(),LoginActivity.class));
+        });
         return root;
     }
 
